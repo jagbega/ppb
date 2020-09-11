70b3f6c98227b80935f06100aecded9ccefce64c
jagbega <jagbega@gmail.com>
2020-09-11 09:27:39 -0300

    Add files via upload

---
+++ fox/assets/js/eventos.js
@@ -0,0 +1,22 @@
+$(function(){
+   // $('#menu').mouseleave (function(){
+        //alert('Teste');
+     //   setInterval(() => {
+            //$('#menu').attr('style','left: -350px');
+            //$('#menu').fadeOut();
+            /*$(this).css({"background":"rgba(16, 16, 16, .5)", "width":"350px",
+            "height":"100%", "left":"-700px", "position":"absolute", "transition":"all .5", "top":"0%"});*/
+       //     $('#menu').fadeOut(500);
+           
+        //}, 2000);
+        //$('.navmenu').css('left','-350px');
+        //$('nav').css('left', '0');
+        /*$(this).css({"background":"rgba(16, 16, 16, .5)", "width":"350",
+        "height":"100%", "left":"0%", "position":"absolute", "transition":"all .5", "top":"0%"});*/ 
+        //$('#menu').off();
+        //$('#menu').off();
+    //});
+    //$('#chec').click(function(){
+      //  $('#menu').fadeIn();
+    //});
+});
---
+++ fox/index.php
@@ -0,0 +1,34 @@
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <title>SiteTeste</title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/><!-- deixa a largura minima de acordo com o dispositivo -->
+    <script type="text/javascript" src="assets\js\jquery-3.1.1.min.js"></script>
+    <script type="text/javascript" src="assets\js\eventos.js"></script>
+    <script type="text/javascript" src="assets\js\bootstrap.min.js"></script>
+    <link rel="sortcut icon" href="assets\images\IcoNav.svg" type="image/x-icon"/>;<!-- type="image/x-icon" -->
+    <link rel="stylesheet" href="assets\css\bootstrap.min.css"/>
+    <link rel="stylesheet" href="assets\css\estilo.css"/> 
+    
+</head>
+<body>
+<input type="checkbox" id="chec" ><!-- checkbox usado para o evente de esconder e mostrar o menu -->
+<label for="chec"><!-- icone do menu -->
+    <img src="assets\images\menu1.png" alt="">
+</label>
+<!-- inicio navegação menu -->
+<nav id="menu" class="">
+    <ul>
+        <li class="active"><a href="">FUNCIONÁRIOS</a></li>
+        <li><a href="">HORAS TRABALHADAS PROJETO</a></li>
+        <li><a href="">FUNÇÃO / CARGO</a></li>
+        <li><a href="">HORAS TRABALHADAS MENSAL</a></li>
+        <li><a href="">BENEFÍCIOS</a></li>
+        <li><a href="">ENCARGOS</a></li>
+        <li><a href="">REVERSÃO P&D</a></li>
+    </ul>
+</nav>
+<!-- fim navegação menu -->
+
+</body>
+</html>
